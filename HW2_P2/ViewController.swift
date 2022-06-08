f121ae21428ed29776db8cd6d81801c8e8faac39
Aleksandr Babkin <plizkinzmey@gmail.com>
2022-06-08 16:14:19 +0300

    init

---
+++ HW2_P2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HW2_P2
+//
+//  Created by Aleksandr Babkin on 08.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        // init
+    }
+
+
+}
+
